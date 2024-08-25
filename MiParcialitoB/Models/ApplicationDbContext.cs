0933b7228fcfebf2433b3cec87cabf80c5885215
Josue30759 <josue30759@gmail.com>
2024-08-24 18:38:06 -0600

    Agregar archivos de proyecto.

---
+++ MiParcialitoB/Models/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿namespace MiParcialitoB.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Estudiante> Estudiantes { get; set; }
+    public DbSet<Curso> Cursos { get; set; }
+    public DbSet<Inscripcion> Inscripciones { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Inscripcion>()
+            .HasKey(i => new { i.EstudianteID, i.CursoID });
+
+        modelBuilder.Entity<Inscripcion>()
+            .HasOne(i => i.Estudiante)
+            .WithMany(e => e.Inscripciones)
+            .HasForeignKey(i => i.EstudianteID);
+
+        modelBuilder.Entity<Inscripcion>()
+            .HasOne(i => i.Curso)
+            .WithMany(c => c.Inscripciones)
+            .HasForeignKey(i => i.CursoID);
+    }
+}
+
